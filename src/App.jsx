7e077ef0252f0a295ee072bee38b4f3f616d1e38
imaosazee4github <86365081+imaosazee4github@users.noreply.github.com>
2023-10-26 10:15:07 +0100

    create header-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Profile from './pages/Profile'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import Header from './components/Header'
 
 
 
 export default function App() {
   return (
-    <div className='text-blue-500'>App Hello</div>
+   
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/about' element={<About />}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/signup' element={<Signup/>}/>
+      </Routes>
+      </BrowserRouter>
   )
 }
